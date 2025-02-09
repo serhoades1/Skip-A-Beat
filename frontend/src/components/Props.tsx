5dc569dacb545fe0e23dc246e1ef63f2ff7de93a
Stephen Rhoades <stephenrhoades04@gmail.com>
2025-02-09 05:37:04 +0000

    reset

--- frontend/src/components/Props.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Music } from 'lucide-react';
-
-interface Props {
-  onConnect: () => void;
-}
--- frontend/src/config/spotify.ts
+++
@@ -1,9 +0,0 @@
-export const SPOTIFY_CLIENT_ID = 'b8e7060c1f7845aeb5f3cdd0a1846550';
-export const REDIRECT_URI = window.location.origin;
-export const SCOPES = [
-  'streaming',
-  'user-read-email',
-  'user-read-private',
-  'user-modify-playback-state',
-  'user-read-playback-state'
-].join(' ');
